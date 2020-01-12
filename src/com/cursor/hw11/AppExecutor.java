2aa0330caecb1bbc2f7a51b52873bad8295e67ae
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2020-01-12 18:49:45 +0200

    Added homework 11

---
+++ src/com/cursor/hw11/AppExecutor.java
@@ -0,0 +1,9 @@
+package com.cursor.hw11;
+
+import java.lang.reflect.Array;
+
+public class AppExecutor {
+    public void runApp() {
+        Array[] arrays = new Array[Integer.MAX_VALUE];
+    }
+}
